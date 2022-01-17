ca1a50118d1d63f3d684eeda9f32531be9769d59
Evelyn Acevedo <alwaysevelyn14@gmail.com>
2022-01-16 21:01:47 -0500

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,9 +1,51 @@
 // The application’s main Chat component that renders the chat UI
 
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, KeyboardAvoidingView, Platform } from 'react-native';
+
+// importing the Gifted Chat Library
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+        }
+    }
+
+    componentDidMount(){
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar:'https://placeimg.com/140/140/any',
+                    },
+                },
+                {
+                    _id: 2,
+                    text: 'This is a system message',
+                    createdAt: new Date(),
+                    system: true,
+                },
+            ]
+        })
+    }
+    
+
+    // A message that a user has just sent gets appended to the state messages
+    // so that it can be displayed in that chat. 
+    onSend(messages = []) {
+        this.setState((previousState) => ({
+          messages: GiftedChat.append(previousState.messages, messages),
+        }));
+      }
+
     render() {
 
         // Get the name that was entered in on Start Screen (state 'name')
@@ -19,10 +61,21 @@ export default class Chat extends React.Component {
         return (
             <View style={{
                 flex:1, 
-                justifyContent:'center', 
-                alignItems:'center',
                 backgroundColor: bgColor ? bgColor : '#fff' }}>
                 <Text>Chat Screen</Text>
+                <View style={{flex:1}}>
+
+                 <GiftedChat
+                    messages={this.state.messages}
+                    onSend={(messages) => this.onSend(messages)}
+                    user={{
+                    _id: 1,
+                    }}
+                />
+
+                    { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height"
+                    /> : null }
+                </View>
             </View>
         )
     }
